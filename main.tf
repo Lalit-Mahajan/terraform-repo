d6765df8e7510422f250f6aaa1db9d9f5b893e9d
Lalit Mahajan <139694592+Lalit-Mahajan@users.noreply.github.com>
2024-10-05 13:32:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "one" {
-  ami                    = "ami-00f251754ac5da7f0"
+  ami                    = "ami-078264b8ba71bc45e"
   instance_type          = "t2.micro"
   key_name               = "lalitmahajan"
   vpc_security_group_ids = ["sg-0ec7273276d113aea"]
