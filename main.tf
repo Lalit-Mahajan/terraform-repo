d9a4d53213c231d079fbe42210c801f9370f8744
Lalit Mahajan <139694592+Lalit-Mahajan@users.noreply.github.com>
2024-10-05 12:27:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+  ami                    = "ami-08718895af4dfa033"
+  instance_type          = "t2.micro"
+  key_name               = "lalitmahajan"
+  vpc_security_group_ids = ["sg-0ec7273276d113aea"]
+  tags = {
+    Name = "lalit01"
+  }
